4ce53e11698341f8e03c7170a3c2363d106b455f
Daniel Teixeira <daniel.t.dt@gmail.com>
2014-03-21 21:30:46 -0300

    Add tests to timon.Model#scoped()
      Actually, only for scoped().findOne

--- test/index_test.js
+++ test/index_test.js
@@ -34,4 +34,56 @@ describe('timon', function () {
             expect(connection.constructor.name).to.be.eql('Connection');
         });
     });
+
+    describe('Model', function () {
+        // Connect to mongo
+        before(function (done) {
+            mongodb.MongoClient.connect(this.mongo, function (err, db) {
+                done(err, this.db = db);
+            }.bind(this));
+        });
+        after(function () { this.db.close(); });
+
+        function DogConstructor(set) {
+            set({property: 'name', type: String});
+            this.bark = function () { return 'woof!'; };
+        }
+
+        function PugConstructor(set) {
+            this.bark = function () { return 'pugs goes woof!'; };
+        }
+
+        before(function () { this.model = new timon.Model('Dog', DogConstructor); });
+
+        beforeEach(function (done) {
+            this.db.collection('dogs').insert({name: 'Pluto'}, done);
+        });
+        afterEach(function (done) {
+            this.db.collection('dogs').remove({}, done);
+        });
+
+        describe('#scoped()', function () {
+            it('returns a plain scoped query object', function () {
+                var scoped = this.model.scoped();
+                expect(scoped._conditions).to.be.eql({});
+            });
+
+            it('returns a scope with discriminator described', function () {
+                var subModel = this.model.extend('subModel', function () { });
+                var subScoped = subModel.scoped();
+
+                expect(subScoped._conditions).to.be.eql({_type: 'subModel'});
+            });
+
+            it('serializes from database as model', function (done) {
+                var DogModel = this.model;
+                this.model.scoped().where('name').equals('Pluto').findOne(function (err, dog) {
+                    if (err) { return done(err); }
+
+                    expect(dog).to.be.an.instanceof(DogModel);
+                    done();
+                });
+            });
+        });
+    });
 });
